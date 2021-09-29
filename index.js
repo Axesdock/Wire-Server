4f930031aa7f79a1516a7300394bf46b0296b558
Abhishek Shingane <abhisheks@iitbhilai.ac.in>
2021-09-30 00:10:59 +0530

    WIP

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const api = require('./api')
 var cors = require('cors')
 const mongoose = require('mongoose');
-
+const Device = require('./models/device');
 
 mongoose.connect('mongodb+srv://admin:IW0BGph6eQOZRQLP@cluster0.unq25.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 mongoose.Promise = global.Promise;
@@ -12,6 +12,40 @@ mongoose.Promise = global.Promise;
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Socketio
+const http = require('http').Server(app);
+const io = require('socket.io')(http,{
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
+  });
+
+var connectedDevicesSocket = [];
+var macSocketMapping = {};
+
+io.sockets.on('connection', function(socket) {
+    connectedDevicesSocket.push(socket);
+
+    socket.on('connect_device',function(data){
+        var mac= data.mac;
+        macSocketMapping[mac] = socket;
+        Device.find({mac:mac}).then(())
+    });
+
+    socket.on('disconnect', function() {
+        console.log('Got disconnect!');
+        var i = connectedDevicesSocket.indexOf(socket);
+        connectedDevicesSocket.splice(i, 1);
+    });
+});
+
+ 
+ http.listen(4009, function() {
+    console.log('listening on *:3000');
+ });
+
 
 // initialize routes
 app.use('/api',api);
