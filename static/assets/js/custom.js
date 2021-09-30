b51459f9db99e5436c3f9a1c444aa9a19c58d1b1
saksham291 <sakshamb@iitbhilai.ac.in>
2021-09-30 15:03:34 +0530

    edit devices completed

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -6,7 +6,7 @@
 //   var userList = new List('list-devices', options);
 
 
-
+// Render dashboard table starts
 async function getDevices() {
   let url = 'http://localhost:4004/api/devices';
   try {
@@ -67,8 +67,8 @@ async function renderDevices() {
                                   <i class="fas fa-ellipsis-v"></i>
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                                  <a class="dropdown-item" href="#">Edit</a>
-                                  <a class="dropdown-item" href="#">Delete</a>
+                                  <a class="dropdown-item" href="addnew.html?id=${device.name}">Edit</a>
+                                  <a class="dropdown-item" href="delete.html?id=${device.name}">Delete</a>
                                 </div>
                               </div>
                             </td>
@@ -85,4 +85,94 @@ async function renderDevices() {
   });
 
 }
+// Render dashboard table ends
 
+
+//edit devices starts
+async function getDevice(id) {
+  let url = 'http://localhost:4004/api/devices/'+id;
+  try {
+      let res = await fetch(url);
+      return await res.json();
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+async function editDevice(id) {
+  let device = await getDevice(id);
+  let html = '';
+    if(!device.isRegistered) {
+
+      html = `   <form id="newDevice-form" action="http://localhost:4004/api/devices/${device.name}">
+                  <h6 class="heading-small text-muted mb-4">Device Details</h6>
+                  <div class="pl-lg-4">
+                    <div class="row">
+                      <div class="col-lg-6">
+                        <div class="form-group">
+                          <label class="form-control-label" for="name">Name</label>
+                          <input type="text" id="name" name="name" class="form-control" placeholder="Name" value="${device.name}">
+                        </div>
+                      </div>
+                      <div class="col-lg-6">
+                        <div class="form-group">
+                          <label class="form-control-label" for="mac">MAC Address</label>
+                          <input type="text" id="mac" name="mac" class="form-control" placeholder="MAC Address" value="${device.mac}">
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="form-group">
+                          <label class="form-control-label" for="installAddress">Location</label>
+                          <input id="installAddress" name="installAddress" class="form-control" placeholder="Installation Location" value="${device.installAddress}" type="text">
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <hr class="my-4" />
+                  <!-- Address -->
+                  <h6 class="heading-small text-muted mb-4">Ports</h6>
+                  <div class="pl-lg-4">
+                    <div class="row">
+                      <div class="col-lg-6">
+                        <div class="form-group">
+                          <label class="form-control-label" for="port1">Port 1</label>
+                          <input type="text" id="port1" name="port1" class="form-control" placeholder="Add Comment" value="${device.port1}">
+                        </div>
+                      </div>
+                      <div class="col-lg-6">
+                        <div class="form-group">
+                          <label class="form-control-label" for="port2">Port 2</label>
+                          <input type="text" id="port2" name="port2" class="form-control" placeholder="Add Comment" value="${device.port2}">
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <hr class="my-4" />
+                  <!-- Description -->
+                  <h6 class="heading-small text-muted mb-4">Other Information</h6>
+                  <div class="pl-lg-4">
+                    <div class="form-group">
+                      <label class="form-control-label">Comment</label>
+                      <textarea rows="4" class="form-control" placeholder="Add any Additional comment." name="comment" value="${device.comment}"></textarea>
+                    </div>
+                  </div>
+                  <div class="text-center">
+                    <button type="submit" class="btn btn-primary mt-4">Add Device</button>
+                  </div>
+                </form>`;
+    }
+
+  let container = document.querySelector('.form-render');
+  container.innerHTML = html;
+  const deviceForm = document.getElementById("newDevice-form");
+  deviceForm.addEventListener("submit", handleFormSubmit);
+  console.log("EventListener Active.");
+}
+//edit devices ends
+
+// dashboard stats starts
+
+
+// dashboard stats ends
